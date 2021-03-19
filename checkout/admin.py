f38033583895b7823d8ad4f82486412163ff80ae
atvars <avelavs@gmail.com>
2021-03-19 14:10:42 +0000

    completed web hook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,13 +12,15 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_code', 'date',
                        'delivery_price', 'total',
-                       'grand_total',)
+                       'grand_total', 'original_cart',
+                       'stripe_pid')
 
     fields = ('order_code', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_price',
-              'total', 'grand_total',)
+              'total', 'grand_total', 'original_cart',
+              'stripe_pid')
 
     list_display = ('order_code', 'date', 'full_name',
                     'total', 'delivery_price',
--- checkout/models.py
+++ checkout/models.py
@@ -22,6 +22,8 @@ class Order(models.Model):
     delivery_price = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _create_order_code(self):
         """
