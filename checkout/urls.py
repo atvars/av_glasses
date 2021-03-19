0d26e3b4d04692526712721a8f00585c47289e47
atvars <avelavs@gmail.com>
2021-03-19 12:13:31 +0000

    added checkout data in intent payment

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('payment_successful/<order_code>', views.payment_successful, name='payment_successful'),
+    path('checkout_data/', views.checkout_data, name='checkout_data'),
+    path('swh/', webhook, name='webhook'),
 ]
