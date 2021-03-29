fd82e9b7f76385a977ce5f46749eebddd098d572
atvars <avelavs@gmail.com>
2021-03-29 13:56:52 +0000

    admin - editing functionality added

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_new_product, name='add_new_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
